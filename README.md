00769b9d930d1fec3670b95b501de61b2baae25c
ObakeTamago <obaketamagoyt@gmail.com>
2025-11-18 16:18:35 -0400

    adciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aprendendo do zero programação clonando o https://www.tabnews.com.br/ apartir de um repositório em branco
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return<h1>Responda. É Molodoy ou não é?</h1>
+  return <h1>Responda. É Molodoy ou não é?</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
